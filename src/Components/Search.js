b3b0a7e01c0df9742ac106bc8bd201b2e92230d1
zprogrammer-code <zprogrammercode@gmail.com>
2022-12-05 18:51:07 -0700

    I am configuring my main search component and function. I am really enjoying all of the different kinds of ways i can go about this. I have started to fall in love with semantic-ui-react because they use their grid components and its property row and column to orient almost everything on the screen and it also it allows your layout to have some pre written css that ensures that your wen page is responsive.'

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,35 +1,54 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import { Input, ItemImage } from 'semantic-ui-react';
+import { Input} from 'semantic-ui-react';
 import { Card } from 'semantic-ui-react';
+import '/home/zprogrammercode/ecommerce-ultra/src/Components/Components.css';
 
 q    
 export function MainSearch(){
+    const [APIData, setAPIData] = useState([]);
+    const [searchInput, setsearchInput] = useState('');
+    useEffect((){
+        axios.get('data.url')
+        .than((response) => {
+            setAPIData(response.data)
+        })
+    })
     return(
         <>
-        <div>
-            <div alt="Search component Navbar">
+        <div className='searchComponentContainer'>
+            <div className='searchComponentgNavbar'>
                 <div alt="container showing number of items">
                     <p> xx.x items</p>
                 </div>
-                <div></div>
+                <div>
+                    <label>Results</label>
+                </div>
             </div>
-            <form>
+            <form> 
                 <Input placeholder='Vape Make'/>
                 <Input placeholder='Vape Model'/>
                 <Input placeholder='E-Juice'/>
                 <Input placeholder='chargers and Apparal'/>
-            <form/>
+            </form>
+            <div>
+                {/*grid*/}
             <Card.Group itemsPerRow={3} style={{marginTop:20}}>
+            {APIData.map((item)=> {
+                
+            })}
                 <Card>
                     <Card.Content>
-                        {ItemImage}
-
+                        {item.Image}
+                        {item.className}
+                        {item.Data}
+                        {item.price}
                     </Card.Content>
                 </Card>
             </Card.Group>
             
         </div>
-        </>
-    )
+        </div>
+    </>
+    );
 }
