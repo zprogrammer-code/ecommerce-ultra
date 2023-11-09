d561c619dc5a6cb3b80c94f530a680f28a86354d
Zachary Jacobs <zprogrammercode@gmail.com>
2023-11-09 08:49:15 -0700

    just finsihed the mock api call for products and am now going changing productcard to include state so that we can initiate the population

---
+++ src/Components/Accordian/Products.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+function products(fail) {
+    return new Promise((resolve, reject) => {
+        setTimeout (() => {
+            if (fail) {
+                reject("epic fail");
+            } else {
+                resolve({
+                    products: [
+                        {id: 0, name: "nike shox", summary: "premiere classic look and model. PRICE: $125", pic: "holder.js/100px160" },
+                        {id: 1, name: "nike jordan", summary: "premiere classic look and model. PRICE: $125", pic: "holder.js/100px160" },
+                        {id: 2, name: "nike running", summary: "premiere classic look and model. PRICE: $125", pic: "holder.js/100px160" },
+                        {id: 3, name: "nike chilling", summary: "premiere classic look and model. PRICE: $125", pic: "holder.js/100px160" }
+                    ]
+                })
+            }
+        })
+    })
+};
+
+export default products;
