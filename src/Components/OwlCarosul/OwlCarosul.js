a08d010bc4d5f7b29938a87fab08ffcad267cda3
Zachary Jacobs <zprogrammercode@gmail.com>
2023-11-30 16:53:34 -0700

    adding login and logout user functionality

--- src/Components/OwlCarosul/OwlCarosul.js
+++ src/Components/OwlCarosul/OwlCarosul.js
@@ -1,26 +1,22 @@
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function OwlCarousel() {
   return (
     <>
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
