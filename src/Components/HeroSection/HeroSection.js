6f8f0f75565ff290b0410d00d0608977c9055ef2
Zachary Jacobs <zprogrammercode@gmail.com>
2023-12-23 15:45:45 -0700

    tried a few different things. relizing now background images dont work component integration

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -2,26 +2,24 @@ import React from 'react';
 import "/home/zprogrammercode/React_ecommerce_ultra2/ecommerce-ultra/src/Components/Components.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button, Image } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 
 
 function HeroSection() {
     return(
         <>
-        <div className='the-real-herosection-container'>
-            
-            
+        <div className='the-real-herosection-container'> 
+           
             <Container className='herosection-content'>
+                
             <div className= 'hero_wrapper'>
-            <Image
-                alt=""
-                src="/home/zprogrammercode/ecommerce-ultra/src/Assets/logo"
-                />
+            
                                 <h1> I love these outfits more every day</h1>
                 <p>Our outfits and styles are personally selected by a head female and male stylist</p>
                 <Button href="Store" type='button' class="btn btn-primary">Shop Merch</Button>
             </div>
+            
             </Container>
         </div>
         </>
