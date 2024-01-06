c6f82cbfdd42268b3b33aaa3e0ef791a3bc1eade
Zachary Jacobs <zprogrammercode@gmail.com>
2024-01-05 22:24:16 -0700

    using localStorage for my cart construction, I am now starting on cart.jsx

--- src/Features/cartSlice.js
+++ src/Features/cartSlice.js
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 
 const initialState = {
-    cartItems: [],
+    cartItems: localStorage.getItem("cartItems")
+     ? JSON.parse(localStorage.getItem("cartItems"))
+     : [],
     cartTotalQuantity: 0,
     cartTotalAmount: 0,
 };
@@ -28,6 +30,8 @@ const cartSlice = createSlice({
                         position: "bottom-left",
                     }); 
                 }
+
+                localStorage.setItem("cartItems",  JSON.stringify(state.cartItems))
 
         },
     },
